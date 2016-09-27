46eccd132401c1150caa30ef3dfc404664e658f0
gnishida <gnishida@purdue.edu>
2016-09-26 21:06:20 -0400

    add silhouette converter to ShiftImage project.

--- ShiftImage/main.cpp
+++ ShiftImage/main.cpp
@@ -4,6 +4,19 @@
 #include <iostream>
 #include <fstream>
 #include <glm/gtx/string_cast.hpp>
+#include <sstream>
+#include <string>
+#include <iomanip>
+
+glm::vec2 convert(const glm::vec2& p, int orig_width, int orig_height, int new_width, int new_height, const glm::vec2& image_offset) {
+	float orig_size = std::max(orig_width, orig_height);
+	float new_size = std::max(new_width, new_height);
+
+	glm::vec2 ret;
+	ret.x = p.x * orig_size / new_size + (float)(new_size - new_width - orig_size + orig_width) / new_size * 400.0f + (float)image_offset.x / new_size * 800.0f;
+	ret.y = p.y * orig_size / new_size + (float)(new_size - new_height - orig_size + orig_height) / new_size * 400.0f + (float)image_offset.y / new_size * 800.0f;
+	return ret;
+}
 
 int main() {
 	boost::filesystem::path dir("data");
@@ -14,8 +27,11 @@ int main() {
 			int id;
 			sscanf(it->path().filename().string().c_str(), "lines_%d.txt", &id);
 
+			// convert id to 2 digits string
+			std::stringstream ss;
+			ss << std::setw(2) << std::setfill('0') << id;			std::string str_id = ss.str();
 			// read image
-			std::string path = std::string("data/") + std::to_string(id) + ".jpg";
+			std::string path = std::string("data/") + str_id + ".jpg";
 			cv::Mat img = cv::imread(path.c_str());
 
 			// read text file
@@ -42,6 +58,29 @@ int main() {
 				}
 			}
 			in.close();
+
+			// read silhouette file
+			std::vector<vp::VanishingLine> silhouette;
+			std::string path2 = std::string("data/") + str_id + ".ctr";
+			if (boost::filesystem::exists(path2)) {
+				std::ifstream in2(path2);
+				while (!in2.eof()) {
+					std::string line;
+					std::getline(in2, line);
+
+					std::vector<std::string> strs;
+					boost::split(strs, line, boost::is_any_of("\t "));
+
+					if (strs.size() == 5) {
+						glm::dvec2 start(std::stof(strs[0]), std::stof(strs[1]));
+						glm::dvec2 end(std::stof(strs[2]), std::stof(strs[3]));
+						if (start != end) {
+							silhouette.push_back(vp::VanishingLine(start.x, start.y, end.x, end.y, std::stoi(strs[4])));
+						}
+					}
+				}
+				in2.close();
+			}
 
 			// compute vanishign points
 			std::vector<glm::dvec2> vps;
@@ -75,13 +114,10 @@ int main() {
 			cv::imwrite((std::string("result/") + std::to_string(id) + ".jpg").c_str(), result);
 
 			// convert the lines
-			center_point.x = center_point.x * (float)image_size / result_size + (float)(result_size - result.cols - image_size + img.cols) / result_size * 400.0f + (float)image_offset.x / result_size * 800.0f;
-			center_point.y = center_point.y * (float)image_size / result_size + (float)(result_size - result.rows - image_size + img.rows) / result_size * 400.0f + (float)image_offset.y / result_size * 800.0f;
+			center_point = convert(center_point, img.cols, img.rows, result.cols, result.rows, image_offset);
 			for (int k = 0; k < lines.size(); ++k) {
-				lines[k].start.x = lines[k].start.x * (float)image_size / result_size + (float)(result_size - result.cols - image_size + img.cols) / result_size * 400.0f + (float)image_offset.x / result_size * 800.0f;
-				lines[k].start.y = lines[k].start.y * (float)image_size / result_size + (float)(result_size - result.rows - image_size + img.rows) / result_size * 400.0f + (float)image_offset.y / result_size * 800.0f;
-				lines[k].end.x = lines[k].end.x * (float)image_size / result_size + (float)(result_size - result.cols - image_size + img.cols) / result_size * 400.0f + (float)image_offset.x / result_size * 800.0f;
-				lines[k].end.y = lines[k].end.y * (float)image_size / result_size + (float)(result_size - result.rows - image_size + img.rows) / result_size * 400.0f + (float)image_offset.y / result_size * 800.0f;
+				lines[k].start = convert(lines[k].start, img.cols, img.rows, result.cols, result.rows, image_offset);
+				lines[k].end = convert(lines[k].end, img.cols, img.rows, result.cols, result.rows, image_offset);
 			}
 
 			// save the converted lines
@@ -91,6 +127,21 @@ int main() {
 				out << lines[k].start.x << " " << lines[k].start.y << " " << lines[k].end.x << " " << lines[k].end.y << " " << lines[k].type << std::endl;
 			}
 			out.close();
+
+			// convert silhouette
+			if (silhouette.size() > 0) {
+				for (int k = 0; k < silhouette.size(); ++k) {
+					silhouette[k].start = convert(silhouette[k].start, img.cols, img.rows, result.cols, result.rows, image_offset);
+					silhouette[k].end = convert(silhouette[k].end, img.cols, img.rows, result.cols, result.rows, image_offset);
+				}
+			}
+
+			// save the converted silhouette
+			std::ofstream out2((std::string("result/") + str_id + ".ctr").c_str());
+			for (int k = 0; k < silhouette.size(); ++k) {
+				out2 << silhouette[k].start.x << " " << silhouette[k].start.y << " " << silhouette[k].end.x << " " << silhouette[k].end.y << " " << silhouette[k].type << std::endl;
+			}
+			out2.close();
 		}
 	}
 
